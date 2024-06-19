6f4575c3e666643a6bfd35d0b54fce8f6e966ab5
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2024-06-19 08:45:27 -0300

    Create exercicio4.py

---
+++ exercicios/primeiros-passos/exercicio4.py
@@ -0,0 +1,10 @@
+# Crie um programa que leia dois números e mostre a soma entre eles. Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+algo = input('Digite algo: ')
+print('O tipo primitivo desse valor é:', type(algo))
+print('É alfabético?', algo.isalpha())
+print('É numérico?', algo.isnumeric())
+print('Só tem espaços?', algo.isspace())
+print('É alfanumérico?', algo.isalnum())
+print('Está em maiúsculas?', algo.isupper())
+print('Está em minúsculas?', algo.islower())
+print('Está capitalizada?', algo.istitle())
